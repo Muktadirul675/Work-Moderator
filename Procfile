910cf68e250e2473ea39ea61298fccb8baf4d668
Muktadirul675 <muktadirul.05@gmail.com>
2021-07-23 15:49:16 +0600

    Initialized

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn moderator.wsgi
--- moderator/settings.py
+++ moderator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -128,3 +129,4 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+django_heroku.settings(locals())
